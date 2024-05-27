bd0c508132c45e277869e3e388e35386a6f21292
RazvanLucacii <rlucaci48@gmail.com>
2024-05-27 13:39:07 +0200

    Add project files.

---
+++ ExamenApiPeliculasAWS/Data/PeliculasContext.cs
@@ -0,0 +1,18 @@
+﻿using ExamenApiPeliculasAWS.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamenApiPeliculasAWS.Data
+{
+    public class PeliculasContext : DbContext
+    {
+        public PeliculasContext(DbContextOptions<PeliculasContext> options)
+            : base(options) { }
+
+        public DbSet<Pelicula> Peliculas { get; set; }
+    }
+}
---
+++ ExamenApiPeliculasAWS/Repositories/RepositoryPeliculas.cs
@@ -0,0 +1,31 @@
+﻿using ExamenApiPeliculasAWS.Data;
+using ExamenApiPeliculasAWS.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamenApiPeliculasAWS.Repositories
+{
+    public class RepositoryPeliculas
+    {
+        private PeliculasContext context;
+
+        public RepositoryPeliculas(PeliculasContext context)
+        {
+            this.context = context;
+        }
+
+        public async Task<List<Pelicula>> GetPeliculasAsync()
+        {
+            return await this.context.Peliculas.ToListAsync();
+        }
+
+        public async Task<List<Pelicula>> GetPeliculasActoresAsync(string actor)
+        {
+            return await this.context.Peliculas.Where(x => x.Actores.Contains(actor)).ToListAsync();
+        }
+    }
+}
